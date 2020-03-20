fddafcb3a4f9aa5682fcae44a545cd06380e4231
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-20 15:40:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,14 +46,15 @@ def naming_system
     sub_category: {
       label: "Phylum",
       sub_category: {
-        label: "Class"
+        label: "Class",
         sub_category: {
-         label: "Order",
-         sub_category: {
-           label: "Family",
-           sub_category: {}
-         }
-       }
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {}
+          }
+        }
       }
     }
+  }
 end
